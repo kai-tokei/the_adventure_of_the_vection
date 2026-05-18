971a75af0c5d72a31b2306eb7418403683424eb0
u3kai <beinosuke01@gmail.com>
2026-05-18 14:43:08 +0900

    fin!!

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pyxel
 import numpy as np
+import cv2
+import mediapipe as mp
+import time
 from dataclasses import dataclass
 from enum import Enum, auto
 
@@ -9,6 +12,15 @@ from structs import *
 
 # 人間の直感に優しい視野角(60度)
 fov_deg = 60
+
+REAL_EYE_DISTANCE = 63.0  # 両眼の感覚（人間の平均は63mm）
+FOCAL_LENGTH_PC_CAMERA = 500.0  # 焦点距離
+
+# MediaPipeの設定
+BaseOptions = mp.tasks.BaseOptions
+FaceDetector = mp.tasks.vision.FaceDetector
+FaceDetectorOptions = mp.tasks.vision.FaceDetectorOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
 
 
 # 実験のモード
@@ -51,20 +63,77 @@ class App:
         # 学校の廊下の画像
         self.hall_img = pyxel.Image(389, 218)
         self.hall_img.load(0, 0, "imgs/hall.png")
-        self.hall_img_point = np.array([[0, 0, 3]])
-        self.hall_img_world_width = 4
+        self.hall_img_point = np.array([[0, -1, 27]])
+        self.hall_img_world_width = 64
+
+        # カメラと顔検出機の初期化
+        options = FaceDetectorOptions(
+            base_options=BaseOptions(
+                model_asset_path="face_models/blaze_face_short_range.tflite"
+            ),
+            running_mode=VisionRunningMode.VIDEO,  # ビデオストリームモード
+        )
+
+        self.detector = FaceDetector.create_from_options(options)
+
+        self.cap = cv2.VideoCapture(0)
 
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        self.camera.pos[2] = pyxel.sin(pyxel.frame_count) * 10 - 8
+        # self.camera.pos[2] = pyxel.sin(pyxel.frame_count) * 10 - 8
 
+        # スペースキーでモードを切り替え
         if pyxel.btnp(pyxel.KEY_SPACE):
             self.experi_mode = (
                 ExperiMode.IMG
                 if self.experi_mode == ExperiMode.DOTS
                 else ExperiMode.DOTS
             )
+
+        if (pyxel.frame_count % 3 == 0) and self.cap.isOpened():
+            success, frame = self.cap.read()
+            if not success:
+                return
+
+            # 画像をRGBに変換し、MediaPipe用に整形
+            frame = cv2.flip(frame, 1)
+            rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
+
+            # AI検出を実行
+            timestamp_ms = int(time.time() * 1000)
+            detection_result = self.detector.detect_for_video(mp_image, timestamp_ms)
+
+            # 顔が検出されたら、顔までの距離を計算
+            if detection_result.detections:
+                detection = detection_result.detections[0]
+                keypoints = detection.keypoints
+
+                if len(keypoints) >= 2:
+                    h, w, _ = frame.shape
+
+                    # keypointsとは、コンピュータビジョンにおける特徴点
+                    # 0: 左目, 1: 右目, 2: 鼻, 3: 口 ...
+                    left_eye = np.array([keypoints[0].x * w, keypoints[0].y * h])
+                    right_eye = np.array([keypoints[1].x * w, keypoints[1].y * h])
+
+                    # 画面上の距離を計算
+                    pixel_distance = np.linalg.norm(left_eye - right_eye)
+
+                    if pixel_distance > 0:
+                        distance_mm = (
+                            REAL_EYE_DISTANCE * FOCAL_LENGTH_PC_CAMERA / pixel_distance
+                        )
+                        distance_cm = distance_mm / 10.0
+
+                        print(distance_cm, 15 - distance_cm * 0.6)
+
+                        # カメラの座標を設定
+                        self.camera.pos[2] = 15 - distance_cm * 0.6
+
+                        # デバッグ用
+                        # self.camera.pos[2] = pyxel.sin(pyxel.frame_count) * 10 - 4
 
     def draw(self):
         pyxel.cls(0)
